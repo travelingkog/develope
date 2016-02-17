0914a80bad7af95c3d92d15247dafc1d5ab2b47e
OHDONGSU <travelingkog@naver.com>
2016-02-17 11:08:25 +0900

    new file:   .classpath 	new file:   .project 	new file:   .settings/org.eclipse.jdt.core.prefs 	new file:   bin/ClassTest.class 	new file:   bin/ClassTest2.class 	new file:   src/ClassTest.java 	new file:   src/ClassTest2.java

---
+++ src/ClassTest.java
@@ -0,0 +1,13 @@
+
+public class ClassTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("test");
+	System.out.println("test2");
+	}
+
+}
